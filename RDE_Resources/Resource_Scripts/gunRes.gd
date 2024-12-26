1369c234b617fc08ee50658424815ada64639536
DasParijat <143519231+DasParijat@users.noreply.github.com>
2024-12-26 14:12:21 -0500

    Account for auto/manual reloading in GunRes

--- RDE_Resources/Resource_Scripts/gunRes.gd
+++ RDE_Resources/Resource_Scripts/gunRes.gd
@@ -7,24 +7,39 @@ class_name GunRes
 
 @export_category("Quantitative Attributes")
 @export var mag_size : int = 30
-@export var fire_rate: float = 0.2       
-@export var reload_time: float = 2.0 # in seconds
-@export var is_automatic: bool = true             
+@export var fire_rate : float = 0.2       
+@export_enum("AUTO", "MANUAL") var reload_type : String = "AUTO"
+@export var reload_time : float = 2.0 # in seconds
+@export var is_automatic : bool = true             
 
 @export_category("Bullet")
 @export var bullet_res : BulletRes   
 @export var num_of_bullets : int
 @export var bullet_spread : float 
 
-var ammo_capacity: int = mag_size
+var cur_ammo: int = mag_size
 
 func shoot():
-	if ammo_capacity > 0:
-		ammo_capacity -= 1
+	if cur_ammo > 0:
+		cur_ammo -= 1
 		return bullet_res.damage
 		
 	return 0
 
+	
 func reload():
-	#add code to wait for reload_time to complete
-	ammo_capacity = mag_size 
+	match(reload_type):
+		"AUTO":
+			auto_reload()
+		"MANUAL":
+			var manual_reload_time = reload_time / mag_size
+			manual_reload(manual_reload_time)
+			
+func auto_reload():
+	#add code to wait for (reload_time) until completely reloaded
+	cur_ammo = mag_size 
+
+func manual_reload(manual_reload_time):
+	while cur_ammo < mag_size:
+		cur_ammo += 1
+		# add code to wait (manual_reload_time) between each bullet loaded
