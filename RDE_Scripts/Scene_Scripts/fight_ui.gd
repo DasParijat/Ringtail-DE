649c3470eec913655dbe0f4f1c984470bc08bfb3
DasParijat <143519231+DasParijat@users.noreply.github.com>
2025-01-08 12:19:51 -0500

    Fixing canvaslayer ui mess
    Pause menu wasn't working so a whole heap of uncommits and recommits were done

--- RDE_Scripts/Scene_Scripts/fight_ui.gd
+++ RDE_Scripts/Scene_Scripts/fight_ui.gd
@@ -1,11 +1,7 @@
 extends Control
 
-
-
 func _ready() -> void:
 	GlobalSignal.connect("get_cur_stats", Callable(self, "_on_get_cur_stats"))
-
-
 
 func _process(delta: float) -> void:
 	pass
@@ -13,4 +9,3 @@ func _process(delta: float) -> void:
 func _on_get_cur_stats(type, stats) -> void:
 	print(type)
 	print(stats)
-	
